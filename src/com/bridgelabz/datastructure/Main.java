1baaa15b99a8358cc224ffcd31237a3d449a9720
Archanagowda23 <archanar249@gmail.com>
2022-03-24 20:57:51 +0530

    addede nodes to the stack

--- src/com/bridgelabz/datastructure/Main.java
+++ src/com/bridgelabz/datastructure/Main.java
@@ -16,5 +16,16 @@ public class Main {
         System.out.println("After deleting element from the queue");
         queue.deQueue();
         queue.display();
+
+        System.out.println();
+        System.out.println("\nWelcome to Stack Problem....!!!");
+        Stack stack = new Stack();
+
+        System.out.println();
+        System.out.println("Element present in the Stack");
+        stack.push(60);
+        stack.push(40);
+        stack.push(50);
+        stack.display();
     }
 }
--- src/com/bridgelabz/datastructure/Stack.java
+++ src/com/bridgelabz/datastructure/Stack.java
@@ -2,4 +2,35 @@ package com.bridgelabz.datastructure;
 
 public class Stack {
 
+    Node top;
+
+    Stack() {
+        this.top = null;
+    }
+    // Pushing element on the top of the stack
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+
+        if (newNode == null) {
+            System.out.println("\nStack Overflow");
+            return;
+        }
+
+        newNode.data = data;
+
+        newNode.next = top;
+
+        top = newNode;
+
+    }
+
+    public void display() { // print method
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.data + "-> ");
+            temp = temp.next;
+        }
+
+    }
 }
