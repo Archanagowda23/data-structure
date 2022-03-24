f97eaff7a45b49f445449bc7da18b6351b7d4284
Archanagowda23 <archanar249@gmail.com>
2022-03-24 20:07:37 +0530

    initial commit

---
+++ src/com/bridgelabz/datastructure/LinkedList.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.datastructure;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to linkedlist program");
+    }
+}
